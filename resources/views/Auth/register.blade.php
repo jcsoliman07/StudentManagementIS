fb4053d9fd2e67bb113af7d0b1c1860e7ead9d55
Jomar Soliman <your-email@example.com>
2025-06-11 11:37:11 +0800

    Initial commit: Laravel project for Student Management IS

---
+++ resources/views/Auth/register.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+
+    <title>Registration</title>
+
+    <!-- This is the Bootstrap -->
+
+    <!-- This is custom CSS -->
+
+
+</head>
+<body>
+
+    <h1> THIS IS REGISTRATION </h1>
+
+
+<!-- This is Script for -->
+    
+</body>
+</html>
